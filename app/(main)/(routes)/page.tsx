8796ac3e01c6b665e8b68be5797e658fc03153be
Zaid Shahab <ZaidBShahab@gmail.com>
2024-05-06 20:50:48 +0500

    Protected the home route through Clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,5 +1,7 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <p className="text-3xl font-bold text-indigo-500">Hello Discord</p>
+    <UserButton afterSignOutUrl="/" />
   );
 }
